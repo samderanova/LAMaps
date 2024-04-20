00bbb26cac6ed2df1785761482caa3be8e52a86c
Aponia <brianmv@uci.edu>
2024-04-20 16:23:00 -0700

    feat: header

--- next-client/src/components/layout/header.tsx
+++ next-client/src/components/layout/header.tsx
@@ -1,67 +1,92 @@
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Ham, User } from "lucide-react";
 import { ThemeToggle } from "../theme/toggle";
+import { getSession } from "@auth0/nextjs-auth0";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-export function Header() {
+const headerNavLinks = [
+  {
+    label: "Demo",
+    href: "/#demo",
+  },
+  {
+    label: "About",
+    href: "/#about",
+  },
+  {
+    label: "Contact",
+    href: "/#contact",
+  },
+];
+
+export async function Header() {
+  const session = await getSession();
+
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Homepage</a>
-            </li>
-            <li>
-              <a>Portfolio</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="navbar-center">
+        <Sheet>
+          <SheetTrigger asChild>
+            <button className="btn btn-ghost flex md:hidden">
+              <Ham />
+            </button>
+          </SheetTrigger>
+
+          <SheetContent side="left">
+            <ul className="menu">
+              {headerNavLinks.map((link) => {
+                return (
+                  <li key={link.href}>
+                    <a href={link.href}>{link.label}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </SheetContent>
+        </Sheet>
+
         <a className="btn btn-ghost text-xl">LAMaps</a>
       </div>
+
+      <div className="navbar-center hidden md:flex">
+        <ul className="menu menu-horizontal px-1">
+          {headerNavLinks.map((link) => {
+            return (
+              <li key={link.href}>
+                <a href={link.href}>{link.label}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
       <div className="navbar-end">
         <ThemeToggle />
+        {session?.user ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <button className="btn btn-ghost btn-circle">
+                <User />
+              </button>
+            </DropdownMenuTrigger>
 
-        <button className="btn btn-ghost btn-circle">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-            <span className="badge badge-xs badge-primary indicator-item"></span>
-          </div>
-        </button>
+            <DropdownMenuContent>
+              Hello, {JSON.stringify(session.user, null, 2)}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <a href="/auth/login" className="btn btn-primary btn-sm">
+            Login
+          </a>
+        )}
       </div>
     </div>
   );
