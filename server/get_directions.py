48fba62649695ce369250a13837675f1e8fc0292
Sam Der <sder@uci.edu>
2024-04-20 00:44:38 -0700

    get_directions return list of latitudes and longitudes along path

--- server/get_directions.py
+++ server/get_directions.py
@@ -1,7 +1,7 @@
 import googlemaps
 from dotenv import load_dotenv
-
 import os
+import pprint
 from datetime import datetime
 
 load_dotenv()
@@ -20,7 +20,23 @@ def get_directions(startLatLon: str, endLatLon: str):
         decimals separated by a comma
     :type endLatLon: string
 
-    :rtype: list of routes
+    :rtype: a list of tuples (start, end, polyline) for each step in each leg
+        of the journey
     """
+
     now = datetime.now()
-    return GMAPS.directions(startLatLon, endLatLon, mode="driving", departure_time=now)
+    result = GMAPS.directions(
+        startLatLon, endLatLon, mode="driving", departure_time=now
+    )
+    if len(result) > 0:
+        result = result[0]
+
+    points = []
+    for leg in result["legs"]:
+        for step in leg["steps"]:
+            points.append([step["start_location"], step["end_location"]])
+
+    return points
+
+
+pprint.pprint(get_directions("34.0544,-118.2441", "34.0544,-117.2441"))
