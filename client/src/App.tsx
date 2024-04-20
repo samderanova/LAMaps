f1ea646e03706f31203b5e4e8273a0d7cc3a89f8
Sam Der <sder@uci.edu>
2024-04-19 21:41:15 -0700

    basic lat lon input form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,35 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl">Hello</h1>
-    </>
-  );
+	async function sendLatLon() { }
+
+	return (
+		<div className="card px-5 py-4 border-2 max-w-xs mx-4 my-3">
+			<h1 className="text-3xl">LAMaps</h1>
+			<p className="my-3">Please input latitude and longitude.</p>
+			<form onSubmit={sendLatLon}>
+				<input
+					type="text"
+					placeholder="Latitude"
+					className="input input-bordered input-success w-full max-w-xs mb-3"
+					pattern="[0-9]+(.[0-9]+)"
+					required
+				/>
+				<br />
+				<input
+					type="text"
+					placeholder="Longitude"
+					className="input input-bordered input-success w-full max-w-xs mb-3"
+					pattern="[0-9]+(.[0-9]+)"
+					required
+				/>
+				<br />
+				<button type="submit" className="btn btn-success">
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
