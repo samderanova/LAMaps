65847ed2c995b28a43d4ce9767e5cc603147b3ee
Eric Pedley <ericpedley@gmail.com>
2024-04-19 23:43:55 -0700

    add option to cap number of points returned

--- server/img_to_points.py
+++ server/img_to_points.py
@@ -26,7 +26,7 @@ def _combine_contours(contours: list[list[tuple[int, int]]]) -> list[tuple[int, 
 
     return combined
 
-def points_from_img(img: cv.Mat) -> list[tuple[int, int]]:
+def points_from_img(img: cv.Mat, points_limit: int = 200) -> list[tuple[int, int]]:
     """
     Returns a list of points from an rgb image
     """
@@ -34,7 +34,35 @@ def points_from_img(img: cv.Mat) -> list[tuple[int, int]]:
     contours, _ = cv.findContours(gray, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     combined_contours = _combine_contours([contour.tolist() for contour in contours])
 
-    return combined_contours
+    if len(combined_contours) <= points_limit:
+        return combined_contours
+
+    neighbor_distances = [
+        np.linalg.norm(np.array(p1) - np.array(p2))
+        for p1, p2 in pairwise(combined_contours)
+    ]
+
+    # TODO: also consider curvature of points to cull. We can get
+    # a rough approximation of curvature by looking at the angle between
+    # the vectors going into and out of the point
+
+    taken_indices = set()
+
+    while len(taken_indices) < points_limit:
+        max_distance_index = np.argmax(neighbor_distances)
+        if max_distance_index in taken_indices:
+            neighbor_distances[max_distance_index] = 0
+            continue
+
+        taken_indices.add(max_distance_index)
+        neighbor_distances[max_distance_index] = 0
+
+    filtered_contours = []
+    for i in range(len(combined_contours)):
+        if i in taken_indices:
+            filtered_contours.append(combined_contours[i])
+
+    return filtered_contours
 
 if __name__ == "__main__":
     img = cv.imread(f"{CURRENT_FILEPATH}/stick_and_triangle.png")
