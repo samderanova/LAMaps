944a76e9b9823290a34c74d8a42265e336927d05
Sam Der <sder@uci.edu>
2024-04-21 03:06:30 -0700

    Use bold font for title

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="navbar bg-base-100 w-full">
-      <a className="bold text-7xl text-center m-auto mt-5" href="/">
+      <a className="font-bold text-7xl text-center m-auto mt-5" href="/">
         LAMaps
       </a>
     </div>
