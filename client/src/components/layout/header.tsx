98f92af5187d189abadc691aed971a0183c2f35c
Sam Der <sder@uci.edu>
2024-04-21 05:35:39 -0700

    Add subtitle to header

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -1,9 +1,14 @@
 export default function Header() {
-  return (
-    <div className="navbar bg-base-100 w-full">
-      <a className="font-bold text-7xl text-center m-auto mt-5" href="/">
-        LAMaps
-      </a>
-    </div>
-  );
+	return (
+		<>
+			<div className="navbar bg-base-100 w-full">
+				<a className="font-bold text-7xl text-center m-auto mt-5" href="/">
+					LAMaps
+				</a>
+			</div>
+			<p className="text-center mt-5">
+				<em>Touching grass, one drawing at a time!</em>
+			</p>
+		</>
+	);
 }
