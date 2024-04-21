f706ecd8eafafb4c18346c81dd06a38be68c0e87
Eric Pedley <ericpedley@gmail.com>
2024-04-21 01:11:34 -0700

    remove unused routes

--- server/routers/maps.py
+++ server/routers/maps.py
@@ -3,49 +3,12 @@ from typing import Annotated
 
 import cv2 as cv
 import numpy as np
-from fastapi import APIRouter, File, Form, UploadFile, status
+from fastapi import APIRouter, File, Form, UploadFile
 from img_to_points import points_from_img
 from make_gpx import b64encode, make_gpx
-from pydantic import BaseModel
-from src.matrix import fit_to_map, get_distance_miles, scale_and_place
+from src.matrix import fit_to_map, scale_and_place
 
 router = APIRouter()
-
-
-class Coordinate(BaseModel):
-    latitude: float
-    longitude: float
-
-
-@router.post("/fit", status_code=status.HTTP_201_CREATED)
-async def fit(coordinates: list[list[float]], starting_point: tuple[float, float]):
-    matrix = scale_and_place(coordinates, starting_point)
-
-    matrix, loss_curve = fit_to_map(matrix)
-
-    return {"coordinates": matrix.tolist(), "loss_curve": loss_curve}
-
-
-@router.post("/distance", status_code=status.HTTP_201_CREATED)
-async def distance(lat: float, lon: float):
-    return get_distance_miles(lat, lon)
-
-
-@router.post("/latlon", status_code=status.HTTP_200_OK)
-async def process_coords(coordinate: Coordinate) -> Coordinate:
-    return Coordinate.model_validate(
-        {"latitude": coordinate.latitude, "longitude": coordinate.longitude}
-    )
-
-
-@router.get("/image", status_code=status.HTTP_200_OK)
-async def get_coords_from_image() -> list[Coordinate]:
-    points = [
-        {"latitude": 34.34, "longitude": -117.234},
-        {"latitude": 34.24, "longitude": -117.334},
-    ]
-    return [Coordinate.model_validate(p) for p in points]
-
 
 @router.post("/coordinatize")
 async def img_to_points(
