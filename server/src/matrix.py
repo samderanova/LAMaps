b77d9fed1c5883ce859c87829a4482067688a433
Eric Pedley <ericpedley@gmail.com>
2024-04-21 00:55:31 -0700

    make shit right-side up

--- server/src/matrix.py
+++ server/src/matrix.py
@@ -3,6 +3,7 @@ from functools import cache
 
 import numpy as np
 import osmnx as ox
+from pyproj import Proj
 
 # Miles per 1 degree.
 
@@ -15,6 +16,12 @@ if LA_MAP_PATH is None:
 def get_map():
     return ox.load_graphml(LA_MAP_PATH)
 
+mercator_projection = Proj(proj="merc", ellps="WGS84")
+def to_mercator(lat: float, lon: float) -> tuple[float, float]:
+    return mercator_projection(lon, lat)
+
+def from_mercator(x: float, y: float) -> tuple[float, float]:
+    return mercator_projection(x, y, inverse=True)
 
 def scale_and_place(
     coordinates: list[list[int]],
@@ -28,17 +35,29 @@ def scale_and_place(
     east = ne[1]
     west = sw[1]
 
-    matrix = np.array(coordinates, dtype=np.float64)
-    origin = np.array([south, west])
+    # convert to mercator
+    north, east = to_mercator(north, east)
+    south, west = to_mercator(south, west)
+    
+    coords_normalized = [
+        (coord[0] / x_max_pixels, 1-coord[1] / y_max_pixels) 
+        for coord in coordinates
+    ]
 
-    lat_multiplier = (north - south) / y_max_pixels
-    lon_multiplier = (east - west) / x_max_pixels
+    coords_scaled = [
+        (
+            south + (north - south) * coord[0], 
+            west + (east - west) * coord[1]
+        )
+        for coord in coords_normalized
+    ]
 
-    matrix[:, 0] = matrix[:, 0] * lat_multiplier
-    matrix[:, 1] = matrix[:, 1] * lon_multiplier
-    matrix = matrix + origin
+    return [
+        tuple(reversed(from_mercator(coord[0], coord[1])))
+        for coord in coords_scaled
+    ]
 
-    return matrix
+
 
 
 def get_loss_matrix(matrix):
