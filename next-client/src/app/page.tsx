7bbeeaef6d41822fe40f3b64a98c060fd264d1f8
Eric Pedley <ericpedley@gmail.com>
2024-04-20 21:17:21 -0700

    add gpx capabilities

--- next-client/src/app/page.tsx
+++ next-client/src/app/page.tsx
@@ -146,6 +146,26 @@ function App() {
     const content = await res.json();
 
     const points: [number, number][] = content.points;
+    const fileEncoded = content.gpxFile;
+
+
+    function saveFile(b64FileContents: string) {
+        // Replace 'your_base64_string' with the actual encoded string from Python
+        const decoded = atob(b64FileContents);
+        const blob = new Blob([decoded], {type: "text/plain;charset=utf-8"});
+        const filename = "encoded_data.gpx";
+
+        // Create a download link
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = filename;
+        link.click();
+
+        // Revoke the object URL to avoid memory leaks
+        URL.revokeObjectURL(link.href);
+    }
+
+    saveFile(fileEncoded)
 
     setWaypoints(points);
 
